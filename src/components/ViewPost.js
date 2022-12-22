522f243ee512566d8accb1998386c963c6aef061
Ntami Egbe <ntamiegbe00@gmail.com>
2022-12-22 18:35:15 +0100

    Commit: Implementing countdown timer for post

--- src/components/ViewPost.js
+++ src/components/ViewPost.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { selectSelectedImage } from '../features/appSlice'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 const ViewPost = () => {
     const navigate = useNavigate()
@@ -14,8 +15,27 @@ const ViewPost = () => {
     }, [selectedImage])
 
     return (
-        <div>
+        <div className='relative'>
             <img src={selectedImage} alt="" />
+            <CountdownCircleTimer
+                isPlaying
+                duration={10}
+                strokeWidth={6}
+                size={50}
+                colors={[
+                    ["#004777", 0.33],
+                    ["#F7B801", 0.33],
+                    ["#A30000", 0.33]
+                ]}
+                className="absolute top-0 right-0 m-3"
+            >
+                {({ remainingTime }) => {
+                    if (remainingTime === 0) {
+                        navigate('/chats')
+                    }
+                    return remainingTime
+                }}
+            </CountdownCircleTimer>
         </div>
     )
 }
